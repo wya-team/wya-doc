e2f01dd51dc5210b841f519050951958ecd55583
Mr.Z <zrd0921@qq.com>
2019-12-13 19:14:29 +0800

    fix: redirect

--- client/src/index.js
+++ client/src/index.js
@@ -36,10 +36,11 @@ router.onReady(() => {
 	delete sessionStorage.redirect;
 
 	// github pages hack
+	// TODO: 需要webpack修改配置
 	const curUrl = `${location.pathname}${location.search}${location.hash}`;
-	if (redirect && redirect.includes(PRE_ROUTER_URL) && redirect != curUrl) {
+	if (redirect && redirect.includes('/wya-doc/site/') && redirect != curUrl) {
 		try {
-			router.push(redirect.replace(PRE_ROUTER_URL, '/'));
+			router.push(redirect.replace('/wya-doc/site/', '/'));
 		} catch (e) {
 			location.href = redirect;
 		}
