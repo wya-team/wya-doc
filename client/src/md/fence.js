d1bf49d44266cbb5907f95acebaec9200b16db8b
ScoutYin <yinbin_web@163.com>
2019-12-22 17:44:36 +0800

    update: fence

--- client/src/md/fence.js
+++ client/src/md/fence.js
@@ -2,19 +2,16 @@ import { PG_MARK } from './constants';
 
 export default md => {
 	const defaultRender = md.renderer.rules.fence;
-	
+
 	md.renderer.rules.fence = (tokens, idx, ...rest) => {
 		const prevToken = tokens[idx - 1];
 		// 如果前一个token是:::[PG_MARK]，则表明这个token是需要被识别为playground的
 		const isHit = prevToken 
 						&& prevToken.nesting === 1 
 						&& prevToken.info.match(new RegExp(`^${PG_MARK}\\s*(.*)$`));
-		if (isHit) {
-			return `
-				<div id="PG-${idx}" data-code="${md.utils.escapeHtml(tokens[idx].content)}"></div>
-			`;
-		} else {
-			return defaultRender(tokens, idx, ...rest);
-		}
+		
+		return isHit
+			? `<div id="PG-${idx}" data-code="${md.utils.escapeHtml(tokens[idx].content)}"></div>`
+			: defaultRender(tokens, idx, ...rest);
 	};
 };
