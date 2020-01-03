0961a77a8c67e448d6562e9459a8d1c1a191aba9
Mr.Z <zrd0921@qq.com>
2020-01-03 10:17:25 +0800

    fix: 'babel-plugin-transform-vue-jsx' export

--- node/config.js
+++ node/config.js
@@ -162,7 +162,7 @@ class Config {
 					vue: 'Vue',
 					lodash: '_',
 					'@babel/standalone': 'Babel',
-					'babel-plugin-transform-vue-jsx': 'babel-plugin-transform-vue-jsx',
+					'babel-plugin-transform-vue-jsx': '"babel-plugin-transform-vue-jsx"',
 					'@wya/vc': 'WYA_VC',
 				}
 				: {}
