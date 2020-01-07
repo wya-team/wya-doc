4879dfa95cd207d44f7d7d950d912f1199bd3b1a
Mr.Z <zrd0921@qq.com>
2020-01-08 00:28:35 +0800

    style: idx -> index

--- client/src/utils/markdown.js
+++ client/src/utils/markdown.js
@@ -35,16 +35,17 @@ config
 const md = config.toMd(markdownIt);
 const defaultRender = md.renderer.rules.fence;
 
-md.renderer.rules.fence = (tokens, idx, ...rest) => {
-	const prevToken = tokens[idx - 1];
+md.renderer.rules.fence = (tokens, index, ...rest) => {
+	const prevToken = tokens[index - 1];
+
 	// 如果前一个token是:::[PG_MARK]，则表明这个token是需要被识别为playground的
 	const isHit = prevToken 
 		&& prevToken.nesting === 1 
 		&& prevToken.info.match(new RegExp(`^${PG_MARK}\\s*(.*)$`));
 	
 	return isHit
-		? `<div id="PG-${idx}" data-code="${md.utils.escapeHtml(tokens[idx].content)}"></div>`
-		: defaultRender(tokens, idx, ...rest);
+		? `<div id="PG-${index}" data-code="${md.utils.escapeHtml(tokens[index].content)}"></div>`
+		: defaultRender(tokens, index, ...rest);
 };
 
 export default md;
